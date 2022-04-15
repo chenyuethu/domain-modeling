f207c44c76e0c64f0fe03ccad2a706d1d978850f
chenyuethu <chen.yue@kellogg.northwestern.edu>
2022-04-15 20:30:49 +0000

    create models

--- crm/crm.sql
+++ crm/crm.sql
@@ -2,4 +2,48 @@
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
 
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS calls;
+DROP TABLE IF EXISTS emails;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS users;
+
+
 -- CREATE TABLES
+
+CREATE TABLE contacts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  phone TEXT,
+  company_id INTEGER
+);
+
+CREATE TABLE calls (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  date TEXT,
+  time TEXT,
+  notes TEXT,
+  contact_id INTEGER
+);
+
+CREATE TABLE emails (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  date TEXT,
+  time TEXT,
+  notes TEXT,
+  contact_id INTEGER
+);
+
+CREATE TABLE companies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+);
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT
+);
